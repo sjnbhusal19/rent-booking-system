6dc30ba5e60f4cc8f74a2056b20d34f9cb8cffb5
sujan <sujanbhushal2014@gmail.com>
2025-04-20 10:01:13 +0545

    inital express setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,11 @@
 import './App.css'
 
 function App() {
-  
-  return(
-    <h1>lets sta4444444444rt..v55444444..........</h1>
+
+  return (
+    <h1>lets sta4444444444rt..v55444444.....
+      jfdasjfk
+      .....</h1>
   )
 }
 export default App
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    message: 'Api are working'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
