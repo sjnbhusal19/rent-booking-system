6d9f170724de106071100121d47ec0b6122df5bd
rahul <rahulagr306@gmail.com>
2025-04-21 13:57:21 +1000

    home page with dummy data created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import PageNotFound from './pages/PageNotFound'
+import HomePage from './pages/HomePage';
+
+
+
 
 function App() {
   
-  return(
-    <h1>lets start..v..</h1>
-  )
+  return (
+    
+    <div className="p-6">
+        
+      <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+    </div>
+  );
 }
 export default App
